d1a0fde09177198cf1e5d0d224fd80a7661533ee
Sanyam Gupta <sanyam233@gmail.com>
2025-05-12 22:05:41 -0700

    Added sockets

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 import json
 import os
+from flask_socketio import SocketIO, emit
+from flask_cors import CORS
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret!"
+CORS(app,resources={r"/*":{"origins":"*"}})
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route("/", methods=["GET"])
 def main():
@@ -14,6 +19,27 @@ def main():
 def home():
     return json.dumps(dict(status="success", message="Working from home"))
 
+@socketio.on("connect")
+def connected():
+    """event listener when client connects to the server"""
+    print(f"client has connected {request.sid}", flush=True)
+    emit("success",{"data":f"id: {request.sid} is connected"}, to=request.sid)
+
+@socketio.on("vm_info")
+def handle_vm_info():
+    print("NEW ENDPOINT", flush=True)
+    name = os.getenv("NAME", "NAME-DEV")
+    zone = os.getenv("ZONE", "ZONE-DEV")
+    emit("vm_info", {"data" : {"name" : name, "zone" : zone}})
+
+@socketio.on("disconnect")
+def disconnected():
+    """event listener when client disconnects to the server"""
+    print(f"user disconnected {request.sid}", flush=True)
+    
+@socketio.on_error()
+def handle_socket_error(err):
+    print("HEREEE", err, flush=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    socketio.run(app, debug=True, host='0.0.0.0', port=8080)
